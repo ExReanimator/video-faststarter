d32aeb2a27558d672da159ac1b0959790908fb0a
Ivan Teplyakov <exreanimator@gmail.com>
2011-11-02 21:33:25 +0400

    Additional comments

--- ruby-video-faststarter.rb
+++ ruby-video-faststarter.rb
@@ -20,13 +20,19 @@ else
     raise ArgumentError, "otherwise the second param must be a destination directory", caller
   end
   rewrite = true
+  
+  # if destination directory hasn't set, use /tmp directory for middle-processing place
   dstdir = '/tmp/'
 end
 
+# for convenience cut last slash of source directory if exist
 srcdir = srcdir[0..-2] if srcdir[-1] == '/'
+
+# and add it for destination directory
 dstdir = dstdir + '/' unless dstdir[-1] == '/'
 
-files = Dir[srcdir + '/*.mp4']
+# read mp4 or mov video
+files = Dir[srcdir + "/*.{mp4,mov}"]
 total = files.count
 
 raise RuntimeError, "Nothing to do in dir: " + srcdir, caller if total == 0
@@ -38,7 +44,10 @@ files.each do |video|
   filename = File.basename(video).to_s
   puts "Remained: " << total.to_s
   puts "-- Processing " << filename
+  
+  # execute qt-faststart from ffmpeg package
   result = %x(qt-faststart #{video} #{dstdir}#{filename})
+  
   if result.to_s =~ /copying rest of file/
     puts "video was successfully faststarted!"
     
